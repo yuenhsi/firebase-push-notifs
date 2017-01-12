1035409bddb7bad6a1dcae2af48c8a5e525d30ef
yuenhsi <yuenhsi93@gmail.com>
2017-01-12 11:50:22 -0800

    add Podfile

---
+++ Pods/Target Support Files/Pods-push-notifs/Pods-push-notifs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_push_notifs : NSObject
+@end
+@implementation PodsDummy_Pods_push_notifs
+@end
---
+++ Pods/Target Support Files/Pods-push-notifs/Pods-push-notifs-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_push_notifsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_push_notifsVersionString[];
+
